81c9cf86fc89dd38cc731b34633357e8ad7a1dff
Wayne Witzel III <wayne@riotousliving.com>
2023-04-10 01:00:22 -0400

    fix session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType<{}> = ({
+const MyApp: AppType = ({
   Component,
   pageProps: { ...pageProps },
 }) => {
