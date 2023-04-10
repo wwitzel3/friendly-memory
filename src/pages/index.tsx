7175d4de2fdcc8ed2583dd0bbc591ae647f43a2e
Wayne Witzel III <wayne@riotousliving.com>
2023-04-10 14:11:55 -0400

    add file routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -88,8 +89,12 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`} </span>
-          <span>{` - ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`} </span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{` - ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span>{post.content}</span>
       </div>
